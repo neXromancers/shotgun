fe61860c6cddcdaaad3849dfa913cec7225c4aef
Dusk Banks <me@bb010g.com>
2020-09-10 17:39:43 -0700

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,8 @@ use std::time;
 use getopts::Options;
 use image::GenericImage;
 use image::Pixel;
-use image::RgbaImage;
 use image::Rgba;
+use image::RgbaImage;
 use x11::xlib;
 
 mod util;
@@ -59,7 +59,10 @@ fn run() -> i32 {
     }
 
     if matches.opt_present("v") {
-        eprintln!("shotgun {}", option_env!("GIT_VERSION").unwrap_or(env!("CARGO_PKG_VERSION")));
+        eprintln!(
+            "shotgun {}",
+            option_env!("GIT_VERSION").unwrap_or(env!("CARGO_PKG_VERSION"))
+        );
         return 0;
     }
 
@@ -79,12 +82,15 @@ fn run() -> i32 {
                 eprintln!("Window ID is not a valid integer");
                 eprintln!("Accepted values are decimal, hex (0x*), octal (0o*) and binary (0b*)");
                 return 1;
-            },
+            }
         },
         None => root,
     };
 
-    let output_ext = matches.opt_str("f").unwrap_or("png".to_string()).to_lowercase();
+    let output_ext = matches
+        .opt_str("f")
+        .unwrap_or("png".to_string())
+        .to_lowercase();
     let output_format = match output_ext.as_ref() {
         "png" => image::ImageOutputFormat::Png,
         "pam" => image::ImageOutputFormat::Pnm(image::pnm::PNMSubtype::ArbitraryMap),
@@ -97,7 +103,8 @@ fn run() -> i32 {
     let window_rect = display.get_window_rect(window);
     let sel = match matches.opt_str("g") {
         Some(s) => match xwrap::parse_geometry(CString::new(s).expect("Failed to convert CString"))
-                         .intersection(window_rect) {
+            .intersection(window_rect)
+        {
             Some(sel) => util::Rect {
                 // Selection is relative to the root window (whole screen)
                 x: sel.x - window_rect.x,
@@ -108,7 +115,7 @@ fn run() -> i32 {
             None => {
                 eprintln!("Invalid geometry");
                 return 1;
-            },
+            }
         },
         None => util::Rect {
             x: 0,
@@ -123,14 +130,16 @@ fn run() -> i32 {
         None => {
             eprintln!("Failed to get image from X");
             return 1;
-        },
+        }
     };
 
     let mut image = match image.into_image_buffer() {
         Some(i) => image::DynamicImage::ImageRgba8(i),
         None => {
-            eprintln!("Failed to convert captured framebuffer, only 24/32 \
-                      bit (A)RGB8 is supported");
+            eprintln!(
+                "Failed to convert captured framebuffer, only 24/32 \
+                      bit (A)RGB8 is supported"
+            );
             return 1;
         }
     };
@@ -144,8 +153,11 @@ fn run() -> i32 {
 
                 // No point in masking if we're only capturing one screen
                 if screens.len() > 1 {
-                    let mut masked = RgbaImage::from_pixel(sel.w as u32, sel.h as u32,
-                                                           Rgba::from_channels(0, 0, 0, 0));
+                    let mut masked = RgbaImage::from_pixel(
+                        sel.w as u32,
+                        sel.h as u32,
+                        Rgba::from_channels(0, 0, 0, 0),
+                    );
 
                     for screen in screens {
                         // Subimage is relative to the captured area
@@ -156,18 +168,19 @@ fn run() -> i32 {
                             h: screen.h,
                         };
 
-                        let mut sub_src = image.sub_image(sub.x as u32, sub.y as u32,
-                                                          sub.w as u32, sub.h as u32);
-                        masked.copy_from(&mut sub_src, sub.x as u32, sub.y as u32)
+                        let mut sub_src =
+                            image.sub_image(sub.x as u32, sub.y as u32, sub.w as u32, sub.h as u32);
+                        masked
+                            .copy_from(&mut sub_src, sub.x as u32, sub.y as u32)
                             .expect("Failed to copy sub-image");
                     }
 
                     image = image::DynamicImage::ImageRgba8(masked);
                 }
-            },
+            }
             None => {
                 eprintln!("Failed to enumerate screens, not masking");
-            },
+            }
         }
     }
 
@@ -183,18 +196,22 @@ fn run() -> i32 {
         None => {
             eprintln!("No output specified, defaulting to {}", ts_path);
             ts_path.as_str()
-        },
+        }
     };
 
     if path == "-" {
-        image.write_to(&mut io::stdout(), output_format).expect("Writing to stdout failed");
+        image
+            .write_to(&mut io::stdout(), output_format)
+            .expect("Writing to stdout failed");
     } else {
         match File::create(&Path::new(&path)) {
-            Ok(mut f) => image.write_to(&mut f, output_format).expect("Writing to file failed"),
+            Ok(mut f) => image
+                .write_to(&mut f, output_format)
+                .expect("Writing to file failed"),
             Err(e) => {
                 eprintln!("Failed to create {}: {}", path, e);
-                return 1
-            },
+                return 1;
+            }
         }
     }
 
