ba5c70bb20ea1d56c4a5df43d467d107f2c7c418
novenary <streetwalkermc@gmail.com>
2023-05-04 15:43:30 +0300

    Fix build when git is not installed
    See: https://github.com/neXromancers/shotgun/issues/35#issuecomment-1534642319

--- build.rs
+++ build.rs
@@ -1,14 +1,12 @@
 use std::process::Command;
 
 fn main() {
-    let git_version = Command::new("git")
-        .arg("describe")
-        .arg("--tags")
-        .output()
-        .expect("Failed to get git description");
-    if git_version.status.success() {
-        let git_version =
-            String::from_utf8(git_version.stdout).expect("Failed to decode git description");
-        print!("cargo:rustc-env=GIT_VERSION={}", git_version);
+    let git_version = Command::new("git").arg("describe").arg("--tags").output();
+    if let Ok(git_version) = git_version {
+        if git_version.status.success() {
+            let git_version =
+                String::from_utf8(git_version.stdout).expect("Failed to decode git description");
+            print!("cargo:rustc-env=GIT_VERSION={}", git_version);
+        }
     }
 }
