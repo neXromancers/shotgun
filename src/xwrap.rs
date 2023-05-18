e79ab18361abbc9967d7fa079b84918670afeb9d
novenary <streetwalkermc@gmail.com>
2023-05-18 13:53:26 +0300

    Port get_image() to x11rb

--- src/xwrap.rs
+++ src/xwrap.rs
@@ -5,7 +5,6 @@
 use std::ffi;
 use std::os::raw;
 use std::ptr;
-use std::slice;
 
 use image::Rgba;
 use image::RgbaImage;
@@ -25,7 +24,12 @@ pub struct Display {
 }
 
 pub struct Image {
-    handle: *mut xlib::XImage,
+    w: u32,
+    h: u32,
+    format: xproto::Format,
+    visual: xproto::Visualtype,
+    byte_order: xproto::ImageOrder,
+    data: Vec<u8>,
 }
 
 impl Display {
@@ -92,22 +96,52 @@ impl Display {
         }
     }
 
-    pub fn get_image(&self, window: xproto::Window, rect: util::Rect) -> Option<Image> {
-        unsafe {
-            let all_planes = !0;
-            let image = xlib::XGetImage(
-                self.handle,
-                window as _,
-                rect.x,
-                rect.y,
-                rect.w as std::ffi::c_uint,
-                rect.h as std::ffi::c_uint,
-                all_planes,
-                xlib::ZPixmap,
-            );
-
-            Image::from_raw_ximage(image)
+    fn find_visual(&self, id: xproto::Visualid) -> Option<&xproto::Visualtype> {
+        for screen in &self.conn.setup().roots {
+            for depth in &screen.allowed_depths {
+                for visual in &depth.visuals {
+                    if visual.visual_id == id {
+                        return Some(visual);
+                    }
+                }
+            }
         }
+        None
+    }
+
+    pub fn get_image(&self, window: xproto::Window, rect: util::Rect) -> Option<Image> {
+        const ALL_PLANES: u32 = !0;
+        let cookie = self
+            .conn
+            .get_image(
+                xproto::ImageFormat::Z_PIXMAP,
+                window,
+                rect.x as i16,
+                rect.y as i16,
+                rect.w as u16,
+                rect.h as u16,
+                ALL_PLANES,
+            )
+            .ok()?;
+        let img = cookie.reply().ok()?;
+
+        let format = *self
+            .conn
+            .setup()
+            .pixmap_formats
+            .iter()
+            .find(|f| f.depth == img.depth)?;
+        let visual = *self.find_visual(img.visual)?;
+        let byte_order = self.conn.setup().image_byte_order;
+
+        Some(Image {
+            w: rect.w as u32,
+            h: rect.h as u32,
+            format,
+            visual,
+            byte_order,
+            data: img.data,
+        })
     }
 
     pub fn get_screen_rects(&self) -> Option<Vec<util::Rect>> {
@@ -157,65 +191,64 @@ impl Drop for Display {
 }
 
 impl Image {
-    fn from_raw_ximage(ximage: *mut xlib::XImage) -> Option<Image> {
-        if ximage.is_null() {
-            None
-        } else {
-            Some(Image { handle: ximage })
-        }
-    }
-
     pub fn to_image_buffer(&self) -> Option<RgbaImage> {
-        let img = unsafe { &*self.handle };
-
-        if (img.red_mask, img.green_mask, img.blue_mask) == (0xF800, 0x07E0, 0x001F) {
+        if (
+            self.visual.red_mask,
+            self.visual.green_mask,
+            self.visual.blue_mask,
+        ) == (0xF800, 0x07E0, 0x001F)
+        {
             return self.to_image_buffer_rgb565();
         }
 
-        let bytes_per_pixel = match (img.depth, img.bits_per_pixel) {
-            (24, 24) => 3,
-            (24, 32) | (32, 32) => 4,
+        let bytes_per_pixel = match (self.format.depth, self.format.bits_per_pixel) {
+            (24, bpp @ 24) | (24 | 32, bpp @ 32) => bpp as u32 / 8,
             _ => return None,
         };
+
+        let pad = match self.format.scanline_pad {
+            p @ 32 => p as u32 / 8,
+            _ => return None,
+        };
+        let bytes_per_line = (self.w * bytes_per_pixel + pad - 1) / pad * pad;
 
         // Compute subpixel offsets into each pixel according the the bitmasks X gives us
         // Only 8 bit, byte-aligned values are supported
         // Truncate masks to the lower 32 bits as that is the maximum pixel size
         macro_rules! channel_offset {
-            ($mask:expr) => {
-                match (img.byte_order, $mask & 0xFFFFFFFF) {
-                    (0, 0xFF) | (1, 0xFF000000) => 0,
-                    (0, 0xFF00) | (1, 0xFF0000) => 1,
-                    (0, 0xFF0000) | (1, 0xFF00) => 2,
-                    (0, 0xFF000000) | (1, 0xFF) => 3,
+            ($mask:expr) => {{
+                use xproto::ImageOrder as O;
+                match (self.byte_order, $mask & 0xFFFFFFFF) {
+                    (O::LSB_FIRST, 0xFF) | (O::MSB_FIRST, 0xFF000000) => 0,
+                    (O::LSB_FIRST, 0xFF00) | (O::MSB_FIRST, 0xFF0000) => 1,
+                    (O::LSB_FIRST, 0xFF0000) | (O::MSB_FIRST, 0xFF00) => 2,
+                    (O::LSB_FIRST, 0xFF000000) | (O::MSB_FIRST, 0xFF) => 3,
                     _ => return None,
                 }
-            };
+            }};
         }
-        let red_offset = channel_offset!(img.red_mask);
-        let green_offset = channel_offset!(img.green_mask);
-        let blue_offset = channel_offset!(img.blue_mask);
-        let alpha_offset = channel_offset!(!(img.red_mask | img.green_mask | img.blue_mask));
-
-        // Wrap the pixel buffer into a slice
-        let size = (img.bytes_per_line * img.height) as usize;
-        let data = unsafe { slice::from_raw_parts(img.data as *const u8, size) };
+        let red_offset = channel_offset!(self.visual.red_mask);
+        let green_offset = channel_offset!(self.visual.green_mask);
+        let blue_offset = channel_offset!(self.visual.blue_mask);
+        let alpha_offset = channel_offset!(
+            !(self.visual.red_mask | self.visual.green_mask | self.visual.blue_mask)
+        );
 
         // Finally, generate the image object
         Some(RgbaImage::from_fn(
-            img.width as u32,
-            img.height as u32,
+            self.w,
+            self.h,
             |x, y| {
-                let offset = (y * img.bytes_per_line as u32 + x * bytes_per_pixel) as usize;
+                let offset = (y * bytes_per_line + x * bytes_per_pixel) as usize;
                 Rgba([
-                    data[offset + red_offset],
-                    data[offset + green_offset],
-                    data[offset + blue_offset],
+                    self.data[offset + red_offset],
+                    self.data[offset + green_offset],
+                    self.data[offset + blue_offset],
                     // Make the alpha channel fully opaque if none is provided
-                    if img.depth == 24 {
+                    if self.format.depth == 24 {
                         0xFF
                     } else {
-                        data[offset + alpha_offset]
+                        self.data[offset + alpha_offset]
                     },
                 ])
             },
@@ -223,25 +256,25 @@ impl Image {
     }
 
     fn to_image_buffer_rgb565(&self) -> Option<RgbaImage> {
-        let img = unsafe { &*self.handle };
-
-        if img.depth != 16 || img.bits_per_pixel != 16 {
+        if self.format.depth != 16 || self.format.bits_per_pixel != 16 {
             return None;
         }
         let bytes_per_pixel = 2;
 
-        // Wrap the pixel buffer into a slice
-        let size = (img.bytes_per_line * img.height) as usize;
-        let data = unsafe { slice::from_raw_parts(img.data as *const u8, size) };
+        let pad = match self.format.scanline_pad {
+            p @ (16 | 32) => p as u32 / 8,
+            _ => return None,
+        };
+        let bytes_per_line = (self.w * bytes_per_pixel + pad - 1) / pad * pad;
 
         // Finally, generate the image object
         Some(RgbaImage::from_fn(
-            img.width as u32,
-            img.height as u32,
+            self.w,
+            self.h,
             |x, y| {
-                let offset = (y * img.bytes_per_line as u32 + x * bytes_per_pixel) as usize;
-                let pixel_slice = [data[offset], data[offset + 1]];
-                let pixel = if img.byte_order == 0 {
+                let offset = (y * bytes_per_line + x * bytes_per_pixel) as usize;
+                let pixel_slice = [self.data[offset], self.data[offset + 1]];
+                let pixel = if self.byte_order == xproto::ImageOrder::LSB_FIRST {
                     u16::from_le_bytes(pixel_slice)
                 } else {
                     u16::from_be_bytes(pixel_slice)
@@ -257,14 +290,6 @@ impl Image {
                 ])
             },
         ))
-    }
-}
-
-impl Drop for Image {
-    fn drop(&mut self) {
-        unsafe {
-            xlib::XDestroyImage(self.handle);
-        }
     }
 }
 
